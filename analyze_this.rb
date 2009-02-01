c14dc25124e926d0f269f543b30afa266a040dc4
Ahmed Sobhi <humanzz@hotmail.com>
2009-02-01 20:41:01 +0200

    initial stuff

---
+++ analyze_this.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'sinatra'
+require 'html_page_data'
+require 'json'
+require 'timeout'
+
+get "/analyze_this" do
+  if params[:url]
+    Timeout::timeout(4) do
+      p = HTMLPageData.get(params[:url])
+      {:error => false, :title => p.title, :description => p.description,
+       :keywords => p.keywords, :host => p.host}.to_json  
+    end
+  else
+    {:error => true, :message => "Care to provide a url?"}.to_json
+  end
+end
+
+error do
+  puts ">>>>>>>>>>#{request.env['sinatra.error']}"
+  {:error => true, :message => "There was an error with your request"}.to_json
+end
